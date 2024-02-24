6ef87d6f73f58555b92c928cc60ea2d81fd8db5a
viktoria <vicktoriakushko@gmail.com>
2024-02-24 12:44:10 +0200

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+import Card from "./Card.jsx";
+function App() {
+
+
+  return (
+      <div className="container">
+          <h1>My App</h1>
+          <Card />
+      </div>
+  )
+}
+
+export default App
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card = () => {
+    return (
+            <div className="card-body">
+                <h5 className="card-title">Card Title</h5>
+                <a href="#" className="btn btn-primary">Link</a>
+            </div>
+    )
+}
+
+export default Card;
